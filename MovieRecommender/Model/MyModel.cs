52965a0383602e8b58a1b4adc7e334b00adf06e9
shahar3 <shahar061@gmail.com>
2017-05-11 20:53:31 +0300

    mnm

--- MovieRecommender/Model/MyModel.cs
+++ MovieRecommender/Model/MyModel.cs
@@ -36,85 +36,16 @@ namespace MovieRecommender.Model
                 //load the movie details from the api
                 loadMoviesFromApi();
             }
-        }
-
-        private void writeToCsvFile(Movie movie, bool first)
-        {
-            StringBuilder sb = new StringBuilder();
-            if (first)
-            {
-                sb.AppendLine("Id,Title,Year,Poster,Rating,Plot");
-            }
             else
             {
-                sb.AppendLine(String.Format("{0},{1},{2},{3},{4},{5}", movie.Id, "\"" + movie.MovieTitle + "\"", movie.Year, movie.Poster, movie.Rating, "\""+movie.Plot+"\"", movie.Genres));
-            }
-            string csvpath = "movies.csv";
-            File.AppendAllText(csvpath, sb.ToString());
-        }
-
-        private void loadMoviesFromApi()
-        {
-            writeToCsvFile(null, true);
-            int i = 0;
-            Movie movie;
-            while (i < moviesDictionary.Count)
-            {
-                moviesToGrab.TryDequeue(out movie);
-                //8207
-                if (i > 8207)
-                {
-                    extractMovieDetails(movie);
-                    writeToCsvFile(movie, false);
-                    Console.WriteLine(i);
-                }
-                i++;
+                //read movies file
+                loadCompleteDatabase();
             }
         }
 
-        private static void extractMovieDetails(Movie movie)
+        private void loadCompleteDatabase()
         {
-            string apiUrl = "http://www.omdbapi.com/?i=" + movie.UrlLink;
-            HttpWebRequest request = WebRequest.Create(apiUrl) as HttpWebRequest;
-            request.Timeout = 1000 * 60 * 30;
-            request.KeepAlive = true;
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            var encoding = ASCIIEncoding.ASCII;
-            using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
-            {
-                string responseText = reader.ReadToEnd();
-                parseJson(responseText, movie);
-            }
-        }
-
-        private static void parseJson(string responseText, Movie movie)
-        {
-            JObject json = JsonConvert.DeserializeObject<dynamic>(responseText);
-            JArray movieRating = (JArray)json.GetValue("Ratings");
-            try
-            {
-                movie.Rating = Movie.extractRating(((JObject)movieRating[0]).GetValue("Value"));
-            }
-            catch (Exception e)
-            {
-                movie.Rating = -1;
-            }
-            try
-            {
-                movie.Poster = (string)json.GetValue("Poster");
-            }
-            catch (Exception e)
-            {
-                movie.Poster = "";
-            }
-            movie.Plot = (string)json.GetValue("Plot");
-        }
-
-        private void loadDatabase()
-        {
-            //read movies.csv
-            using (var fs = File.OpenRead(@"db/movies.csv"))
+            using (var fs = File.OpenRead(@"movies.csv"))
             using (var reader = new StreamReader(fs))
             {
                 using (var csvReader = new CsvReader(reader, true))
@@ -124,35 +55,130 @@ namespace MovieRecommender.Model
                         //the first line is the headers line (movieID,Title,genres)
                         Movie movie = new Movie(csvReader[0]);
                         movie.MovieTitle = csvReader[1];
-                        movie.Genres = Movie.splitToGenres(csvReader[2]);
+                        movie.Year = Int32.Parse(csvReader[2]);
+                        movie.Poster = csvReader[3];
+                        movie.Rating = Double.Parse(csvReader[4]);
+                        movie.Plot = csvReader[5];
                         moviesDictionary[csvReader[0]] = movie;
-                        moviesToGrab.Enqueue(movie);
                     }
                 }
             }
 
-            using (var fs = File.OpenRead(@"db/links.csv"))
-            using (var reader = new StreamReader(fs))
+            private void writeToCsvFile(Movie movie, bool first)
             {
-                using (var csvReader = new CsvReader(reader, true))
+                StringBuilder sb = new StringBuilder();
+                if (first)
                 {
-                    while (csvReader.ReadNextRecord())
+                    sb.AppendLine("Id,Title,Year,Poster,Rating,Plot");
+                }
+                else
+                {
+                    sb.AppendLine(String.Format("{0},{1},{2},{3},{4},{5}", movie.Id, "\"" + movie.MovieTitle + "\"", movie.Year, movie.Poster, movie.Rating, "\"" + movie.Plot + "\"", movie.Genres));
+                }
+                string csvpath = "movies.csv";
+                File.AppendAllText(csvpath, sb.ToString());
+            }
+
+            private void loadMoviesFromApi()
+            {
+                writeToCsvFile(null, true);
+                int i = 0;
+                Movie movie;
+                while (i < moviesDictionary.Count)
+                {
+                    moviesToGrab.TryDequeue(out movie);
+                    //8207
+                    if (i > 8207)
                     {
-                        if (moviesDictionary.ContainsKey(csvReader[0]))
+                        extractMovieDetails(movie);
+                        writeToCsvFile(movie, false);
+                        Console.WriteLine(i);
+                    }
+                    i++;
+                }
+            }
+
+            private static void extractMovieDetails(Movie movie)
+            {
+                string apiUrl = "http://www.omdbapi.com/?i=" + movie.UrlLink;
+                HttpWebRequest request = WebRequest.Create(apiUrl) as HttpWebRequest;
+                request.Timeout = 1000 * 60 * 30;
+                request.KeepAlive = true;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                var encoding = ASCIIEncoding.ASCII;
+                using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
+                {
+                    string responseText = reader.ReadToEnd();
+                    parseJson(responseText, movie);
+                }
+            }
+
+            private static void parseJson(string responseText, Movie movie)
+            {
+                JObject json = JsonConvert.DeserializeObject<dynamic>(responseText);
+                JArray movieRating = (JArray)json.GetValue("Ratings");
+                try
+                {
+                    movie.Rating = Movie.extractRating(((JObject)movieRating[0]).GetValue("Value"));
+                }
+                catch (Exception e)
+                {
+                    movie.Rating = -1;
+                }
+                try
+                {
+                    movie.Poster = (string)json.GetValue("Poster");
+                }
+                catch (Exception e)
+                {
+                    movie.Poster = "";
+                }
+                movie.Plot = (string)json.GetValue("Plot");
+            }
+
+            private void loadDatabase()
+            {
+                //read movies.csv
+                using (var fs = File.OpenRead(@"db/movies.csv"))
+                using (var reader = new StreamReader(fs))
+                {
+                    using (var csvReader = new CsvReader(reader, true))
+                    {
+                        while (csvReader.ReadNextRecord())
                         {
-                            moviesDictionary[csvReader[0]].UrlLink = "tt" + csvReader[1]; //add the IMDB link
+                            //the first line is the headers line (movieID,Title,genres)
+                            Movie movie = new Movie(csvReader[0]);
+                            movie.MovieTitle = csvReader[1];
+                            movie.Genres = Movie.splitToGenres(csvReader[2]);
+                            moviesDictionary[csvReader[0]] = movie;
+                            moviesToGrab.Enqueue(movie);
+                        }
+                    }
+                }
+
+                using (var fs = File.OpenRead(@"db/links.csv"))
+                using (var reader = new StreamReader(fs))
+                {
+                    using (var csvReader = new CsvReader(reader, true))
+                    {
+                        while (csvReader.ReadNextRecord())
+                        {
+                            if (moviesDictionary.ContainsKey(csvReader[0]))
+                            {
+                                moviesDictionary[csvReader[0]].UrlLink = "tt" + csvReader[1]; //add the IMDB link
+                            }
                         }
                     }
                 }
             }
-        }
 
-        private void notifyPropertyChanged(string propName)
-        {
-            if (this.PropertyChanged != null)
+            private void notifyPropertyChanged(string propName)
             {
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+                }
             }
         }
     }
-}
