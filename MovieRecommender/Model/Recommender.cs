6182fa2b8500f99a38c7530413272dd88f728076
daniel verman <daniverman@gmail.com>
2017-05-11 23:48:13 +0300

    ss

--- MovieRecommender/Model/Recommender.cs
+++ MovieRecommender/Model/Recommender.cs
@@ -12,9 +12,11 @@ namespace MovieRecommender.Model
     {
         private Dictionary<string, Movie> m_moviesDictionary;
         private Dictionary<string, User> m_usersDict = new Dictionary<string, User>();
+        private List<string> m_CommonlMovies;
 
         public Recommender(Dictionary<string, Movie> moviesDictionary)
         {
+            m_CommonlMovies = new List<string>();
             this.m_moviesDictionary = moviesDictionary;
             loadUsers();
         }
@@ -43,6 +45,96 @@ namespace MovieRecommender.Model
                     }
                 }
             }
+        }
+
+        private List<string> suggestList(User user)
+        {
+            List<string> ans = new List<string>();
+            Dictionary<string, double> UserSimilarity = new Dictionary<string, double>();
+            double w = 0;
+            foreach (User neighbor in m_usersDict.Values)
+            {
+                m_CommonlMovies.Clear();
+                w = PearsonCorrealation(user, neighbor);
+                UserSimilarity.Add(neighbor.UserId.ToString(), w);
+            }
+            ans = topTenSim(UserSimilarity);
+            return ans;
+        }
+
+        private List<string> topTenSim(Dictionary<string, double> userSimilarity)
+        {
+            var sortedDict = from entry in userSimilarity orderby entry.Value descending select entry;
+            User first = m_usersDict[sortedDict.First().Key];
+            List<string> ans = new List<string>();
+            ans = first.getTopTenMovies();
+            return ans;
+        }
+
+        private double PearsonCorrealation(User user, User neighbor)
+        {
+            double ans = 0;
+            double m = calculateM(user, neighbor);
+            double Ra = calculateR(user);
+            double Ru = calculateR(neighbor);
+            double numerator = calculateNumerator(user, neighbor, Ra, Ru, m);
+            double denominator = calculateDenominator(user, neighbor, Ra, Ru, m);
+            ans = numerator / denominator;
+            return ans;
+        }
+
+        private double calculateDenominator(User user, User neighbor, double ra, double ru, double m)
+        {
+            double ans = 0;
+            double left = 0;
+            double right = 0;
+            foreach (string movieId in m_CommonlMovies)
+            {
+                left += Math.Pow((user.UserMovies[movieId] - ra), 2);
+            }
+            foreach (string movieId in m_CommonlMovies)
+            {
+                right += Math.Pow((neighbor.UserMovies[movieId] - ru), 2);
+            }
+            ans = Math.Sqrt((left * right));
+            return ans;
+        }
+
+        private double calculateNumerator(User user, User neighbor, double ra, double ru, double m)
+        {
+            double ans = 0;
+            double userNu;
+            double neighborNu;
+            foreach (string movieId in m_CommonlMovies)
+            {
+                userNu = user.UserMovies[movieId] - ra;
+                neighborNu = neighbor.UserMovies[movieId] - ru;
+                ans += userNu * neighborNu;
+            }
+            return ans;
+        }
+
+        private double calculateR(User user)
+        {
+            double ans = 0;
+            double sum = user.UserMovies.Values.Sum();
+            double count = user.UserMovies.Keys.Count();
+            ans = sum / count;
+            return ans;
+        }
+
+        private double calculateM(User user, User neighbor)
+        {
+            double ans = 0;
+            foreach (string moviId in user.UserMovies.Keys)
+            {
+                if (neighbor.UserMovies.ContainsKey(moviId))
+                {
+                    ans++;
+                    m_CommonlMovies.Add(moviId);
+                }
+            }
+            return ans;
         }
     }
 }
